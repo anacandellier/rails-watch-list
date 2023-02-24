18b5449df8b04dd6693d7574c3446a461e7db4b2
Ana Candellier <ana.candellier@gmail.com>
2023-02-24 11:22:16 +0100

    changing root

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
 
-  # # A user can see all the lists
-  # get "lists", to: "lists#index"
-  # # A user can create a new list
-  # get "lists/new", to: 'lists#new'
-  # post "lists", to: "lists#create"
-  # # A user can see the details of a given list and its name
-  # get "lists/:id", to: "lists#show", as: "show_list", do
-  #   get 'bookmarks/new', to: "bookmarks#new"
-  #   post "bookmarks", to: "bookmarks#create"
-  # end
-  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
 
  resources :lists, only: [:index, :new, :create, :show] do
   resources :bookmarks, only: [:create]
