7862f2ac4550a74ed2f261b2cadbf6712e0dbb60
Ana Candellier <ana.candellier@gmail.com>
2023-02-24 11:16:32 +0100

    last update

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # A user can see all the lists
+  # get "lists", to: "lists#index"
+  # # A user can create a new list
+  # get "lists/new", to: 'lists#new'
+  # post "lists", to: "lists#create"
+  # # A user can see the details of a given list and its name
+  # get "lists/:id", to: "lists#show", as: "show_list", do
+  #   get 'bookmarks/new', to: "bookmarks#new"
+  #   post "bookmarks", to: "bookmarks#create"
+  # end
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
+
+ resources :lists, only: [:index, :new, :create, :show] do
+  resources :bookmarks, only: [:create]
+ end
+ resources :bookmarks, only: [:destroy]
 end
